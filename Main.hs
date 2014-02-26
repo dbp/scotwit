26dfff59b49869af4d3da624766c913893e8119e
Daniel Patterson <dbp@riseup.net>
2014-02-26 00:09:22 -0500

    better api

--- Main.hs
+++ Main.hs
@@ -5,11 +5,17 @@ import Web.Scotty
 import Control.Monad.Trans (liftIO)
 import Data.Monoid (mconcat)
 import qualified Data.Text.Lazy as LT
+import qualified Data.Text as T
+import Web.Twitter.Conduit
 
 import Twitter
 
 main :: IO ()
 main = scotty 3000 $ do
   get "/" $ do
-    ts <- liftIO tweets
-    html $ mconcat (map LT.fromStrict ts)
+    ts <- liftIO $ run $ take100 homeTimeline
+    let statii = map (\stat ->
+                        let sn = userScreenName . statusUser $ stat
+                            tweet = statusText stat in
+                        T.concat [ sn, ": ", tweet, "\n"]) ts
+    html $ mconcat (map LT.fromStrict statii)
--- Twitter.hs
+++ Twitter.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, ConstraintKinds #-}
 module Twitter where
 
 import Web.Twitter.Conduit
+import Web.Twitter.Conduit.Api
 import Web.Authenticate.OAuth (OAuth(..), Credential(..))
 import qualified Web.Authenticate.OAuth as OA
 import Network.HTTP.Conduit
@@ -14,27 +15,25 @@ import Data.Default
 import Control.Monad.Logger
 import Control.Monad.Trans.Control
 import Control.Monad.Trans.Resource
-import Control.Monad.IO.Class (liftIO)
-import System.IO (hFlush, stdout)
+import qualified Network.HTTP.Types as HT
 
 import qualified Secret as Secret
 
 tokens :: OAuth
 tokens = twitterOAuth
-    { oauthConsumerKey = Secret.cons
-    , oauthConsumerSecret = Secret.cons_sec
+    { oauthConsumerKey = Secret.consumer_tok
+    , oauthConsumerSecret = Secret.consumer_tok_sec
     }
 
 withCredential :: TW (ResourceT (LoggingT IO)) b -> LoggingT IO b
 withCredential task = do
-    let cred = OA.newCredential Secret.tok Secret.tok_sec
+    let cred = OA.newCredential Secret.access_tok Secret.access_tok_sec
     let env = setCredential tokens cred def
     runTW env task
 
-tweets :: IO [T.Text]
-tweets = runStderrLoggingT . withCredential $ do
-    statii <- homeTimeline [] C.$$ CL.take 100
-    return (map (\status ->
-      let sn = userScreenName . statusUser $ status
-          tweet = statusText status in
-      T.concat [ sn, ": ", tweet, "\n"]) statii)
+run :: TW (ResourceT (LoggingT IO)) a -> IO a
+run a = runStderrLoggingT . withCredential $ a
+
+take100 :: (HT.SimpleQuery -> C.Source (TW (ResourceT (LoggingT IO))) a)
+        -> TW (ResourceT (LoggingT IO)) [a]
+take100 a = a [] C.$$ CL.take 100
