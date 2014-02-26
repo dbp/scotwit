96627e7f1d93154f4a2eb0eda3d440379159a06e
Daniel Patterson <dbp@riseup.net>
2014-02-26 00:21:22 -0500

    real newlines, and slightly different imports

--- Main.hs
+++ Main.hs
@@ -6,16 +6,16 @@ import Control.Monad.Trans (liftIO)
 import Data.Monoid (mconcat)
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text as T
-import Web.Twitter.Conduit
+import qualified Web.Twitter.Conduit as Twitter
 
 import Twitter
 
 main :: IO ()
 main = scotty 3000 $ do
   get "/" $ do
-    ts <- liftIO $ run $ take100 homeTimeline
+    ts <- liftIO $ run $ take100 Twitter.homeTimeline
     let statii = map (\stat ->
-                        let sn = userScreenName . statusUser $ stat
-                            tweet = statusText stat in
-                        T.concat [ sn, ": ", tweet, "\n"]) ts
+                        let sn = Twitter.userScreenName . Twitter.statusUser $ stat
+                            tweet = Twitter.statusText stat in
+                        T.concat [ sn, ": ", tweet, "<br/>"]) ts
     html $ mconcat (map LT.fromStrict statii)
